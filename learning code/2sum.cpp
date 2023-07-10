0bb50891f338652f9ea2f5768e07f6206fdc0089
maroonface <ravi.du96@gmail.com>
2023-07-10 23:23:17 +0530

    craeted 2sum cpp

---
+++ learning code/2sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int find(int arr, int size, int target){
+
+}
+
+int main(){
+    int arr[] = {2,7,11,15};
+    int target = 9;
+    unordered_map<int, int> umap;
+    umap[0]=10;
+    umap[1]=20;
+    for(auto i:umap){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    return 0;
+}
